874be09abc1b1570c8d32de6727ef8bee9dd7b9b
VasiliyMelnik64 <vasiliymelnik64@gmail.com>
2023-02-25 10:41:54 +0100

    added redux-persist

--- src/app/providers/with-store.tsx
+++ src/app/providers/with-store.tsx
@@ -1,5 +1,7 @@
 import { Provider } from 'react-redux';
-import { store } from '../store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from '../store';
 
 type Props = {
   [key: string]: any;
@@ -9,6 +11,8 @@ export const withStore =
   (Component: () => React.ReactElement) => (props: Props) =>
     (
       <Provider store={store}>
-        <Component {...props} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...props} />
+        </PersistGate>
       </Provider>
     );
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,9 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistCombineReducers, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { currencyModel } from 'entities/currency';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducer = {
+  currency: currencyModel.currencySlice.reducer,
+};
+
+const reducers = persistCombineReducers(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: {
-    currency: currencyModel.currencySlice.reducer,
-  },
+  reducer: reducers,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
